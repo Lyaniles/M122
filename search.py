335dc12a307844f5ff2dedf879ec8397d3ac7fee
Lyaniles <scholli1501@gmail.com>
2026-01-09 10:54:26 +0100

    Added anti Captcha features and added pagehtml print. TODO finalising html print

--- search.py
+++ search.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time
+from bs4 import BeautifulSoup
 
 # 1. Pfad zum Brave-Browser
 BRAVE_PATH = r"C:\Users\monsi\AppData\Local\BraveSoftware\Brave-Browser\Application\brave.exe"
@@ -10,6 +11,7 @@ BRAVE_PATH = r"C:\Users\monsi\AppData\Local\BraveSoftware\Brave-Browser\Applicat
 options = Options()
 options.binary_location = BRAVE_PATH
 options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-blink-features=AutomationControlled")
 
 # 3. WebDriver starten 
 # Wir lassen den Pfad einfach leer - Selenium sucht sich den Treiber jetzt selbst!
@@ -21,6 +23,8 @@ try:
     # Test-Aufruf
     driver.get("https://www.google.com/search?q=lead+generation+tools&oq=lead+generation+tools")
     print("Erfolg! Brave läuft.")
+    page_html = driver.page_source
+    print(page_html)
     
     time.sleep(5) # Damit wir kurz schauen können
 
